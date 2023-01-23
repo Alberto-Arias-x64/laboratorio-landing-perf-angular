c01398fa0669f82d47204fad4d9f38491f83384c
x64 <alberto.arias.x64@gmail.com>
2023-01-23 09:21:41 -0500

    timeago + img optimization

--- src/app/components/customers/customers.component.ts
+++ src/app/components/customers/customers.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { SwiperOptions } from 'swiper';
 import { SwiperComponent } from 'swiper/angular';
-import * as moment from 'moment';
+import { format } from 'timeago.js';
 
 interface Customer {
   title: string;
@@ -34,14 +34,14 @@ export class CustomersComponent {
       date: new Date(2022, 1, 1),
       text: `Iterative approaches to corporate strategy foster collaborative
   thinking to further the overall value proposition.`,
-      image: 'assets/images/customer-1.png',
+      image: 'assets/images/customer-1.jpg',
     },
     {
       title: 'Oasis has brought clarity to our architecture',
       date: new Date(2022, 3, 1),
       text: `Iterative approaches to corporate strategy foster collaborative
       thinking to further the overall value proposition.`,
-      image: 'assets/images/customer-2.png',
+      image: 'assets/images/customer-2.jpg',
     },
   ];
 
@@ -57,6 +57,6 @@ export class CustomersComponent {
   }
 
   calcTimeAgo(date: Date) {
-    return moment(date).fromNow();
+    return format(date);
   }
 }
